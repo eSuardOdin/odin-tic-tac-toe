7ad250f3cfb2c8165018538657cfb1b531b8d603
eSuardOdin <esuardodin@gmail.com>
2023-01-18 20:34:14 +0100

    Restructuring game architecture

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const Game = (() => {
         [3,6,9]
     ];
     let isPlayerOneTurn = true;
-
+    let winner = '';
 
     const Gameboard = (() => {
         let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -21,16 +21,56 @@ const Game = (() => {
         const getGameboard = () => {
             return gameboard;
         }
-        const setCell = (x, y, symbol) =>{
-            const htmlPosId = gameboard[x][y];
-            gameboard[x][y] = symbol;
+        const setCell = (x,symbol) =>{
+            const htmlPosId = gameboard[x];
+            gameboard[x] = symbol;
             return htmlPosId;
         }; 
         const resetBoard = () => gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
         return {getGameboard, setCell, resetBoard}
     })();
     
-    
+
+
+
+    const GameLogic =(() => {
+
+        /**
+         * Checks winning combinations against a player's moves played, set the winner if matching
+         * @param {*} player Player object who's moves are checked if matching with any winning combination
+         */
+        const checkWinner = (player) => {
+            winningCombinations.forEach(combination => 
+                {
+                    if (player.getMovesPlayed().indexOf(combination[0]) !== -1 &&
+                    player.getMovesPlayed().indexOf(combination[1]) !== -1 &&
+                    player.getMovesPlayed().indexOf(combination[2]) !== -1) 
+                    {
+                        player.setIsWinner();
+                        winner = player.getName();
+                        console.log(`${player.getName()} wins !`);
+                        return;
+                    }
+
+            });
+            console.log(player);
+        }
+        
+        const playTurn = (square) => {
+            if(winner !== '') return;
+            const player = isPlayerOneTurn ? p1 : p2;
+            if(Gameboard.getGameboard().indexOf(Number(square.getAttribute('id'))) !== -1 && square.innerHTML === '') {
+                
+                square.innerHTML = player.getSymbol();
+                player.addMove(Number(square.getAttribute('id')));
+                checkWinner(player);
+                isPlayerOneTurn = ! isPlayerOneTurn;
+            }
+
+        };
+
+        return {checkWinner,playTurn};
+    }) (); 
     
     /**
      * 
@@ -42,20 +82,20 @@ const Game = (() => {
         let _isWinner = false;
         // Check if win (maybe not legitimate to be there)
 
-        const checkIfWin = () => {
-            winningCombinations.forEach(combination => {
-                if (_movesPlayed.indexOf(combination[0]) !== -1 &&
-                    _movesPlayed.indexOf(combination[1]) !== -1 &&
-                    _movesPlayed.indexOf(combination[2]) !== -1) 
-                    {
-                        _isWinner = true;
-                        console.log(`${p1.name} wins !`);
-                        return;
-                }
+        // const checkIfWin = () => {
+        //     winningCombinations.forEach(combination => {
+        //         if (_movesPlayed.indexOf(combination[0]) !== -1 &&
+        //             _movesPlayed.indexOf(combination[1]) !== -1 &&
+        //             _movesPlayed.indexOf(combination[2]) !== -1) 
+        //             {
+        //                 _isWinner = true;
+        //                 console.log(`${getName()} wins !`);
+        //                 return;
+        //         }
 
-            });
-            console.log(`${p1.name} not winning yet`);
-        }
+        //     });
+        //     if (!_isWinner) console.log(`${getName()} not winning yet`);
+        // }
 
         
         // An array of moves the player did
@@ -71,14 +111,80 @@ const Game = (() => {
         const getSymbol = () => {
             return _symbol;
         }
-        return {getName, getSymbol, addMove, getMovesPlayed, checkIfWin};
+        const setIsWinner = () => {
+            _isWinner = !_isWinner;
+        }
+        return {getName, getSymbol, addMove, getMovesPlayed, setIsWinner};
     }
 
     /**
      * For visual display of the game
      */
-    const displayController = (() => {
+//     const displayController = (() => {
+    
+
+
+//         const printMark = (el, player) => {
+//             el.innerHtml = player.getSymbol();
+//         }
+
+
+//         const squares = document.querySelectorAll('.square');
+//         squares.forEach(square => {
+//             square.addEventListener('click', (e) => {
+//                 GameLogic.playTurn(square);
+//             });
+//         })
+
+//         // Print the board
+//         const printBoard = () => {
+//             for(let i = 1; i <= getGameboard().length; i++) {
+
+//             }
+//         };
         
+//     });
+    
+//     // Changes the choosed square in the array location specified
+//     // and changes the player turn
+//     // const playerMove = (x,y) => {
+//     //     if(isPlayerOneTurn) {
+            
+//     //         Gameboard.setCell(x, y, p1.getSymbol())
+//     //     } else {
+//     //         Gameboard.setCell(x, y, p2.getSymbol())
+//     //     }
+//     //     isPlayerOneTurn = !isPlayerOneTurn;
+//     //     return {playerMove}
+//     // };
+//     const p1 = playerFactory('Player one', 'X');
+//     const p2 = playerFactory('Player two', 'O');
+
+    
+    
+    
+
+
+//     return {winningCombinations, displayController};
+})();
+
+
+// console.log(Game.winningCombinations);
+// Game.playerMove(2,2);
+// Game.playerMove(1,1);
+
+
+// Game.displayController();
+
+
+
+
+
+
+
+
+
+    
         // const gameContainer = document.querySelector('.game-container');
         // // On Game.Reset (?) or new game
         // const createBoard = () => {
@@ -90,68 +196,3 @@ const Game = (() => {
         //     gameContainer.insertAdjacentHTML("beforeend", html);
         // }
         // createBoard();
-
-
-
-
-        const squares = document.querySelectorAll('.square');
-        squares.forEach(square => {
-            square.addEventListener('click', (e) => {
-                if(Gameboard.getGameboard().indexOf(Number(square.getAttribute('id'))) !== -1 && square.innerHTML === '') {
-                    square.innerHTML = isPlayerOneTurn ? p1.getSymbol() : p2.getSymbol();
-                    if(isPlayerOneTurn) {
-                        square.innerHTML = p1.getSymbol();
-                        p1.addMove(Number(square.getAttribute('id')));
-                        p1.checkIfWin();
-                    } else {
-                        square.innerHTML = p2.getSymbol();
-                        p2.addMove(Number(square.getAttribute('id')));
-                        p2.checkIfWin();
-                    }
-                    console.log(p1.getMovesPlayed());
-                    console.log(p2.getMovesPlayed());
-                    isPlayerOneTurn = ! isPlayerOneTurn;
-
-                }
-            });
-        })
-
-        // Print the board
-        const printBoard = () => {
-            for(let i = 1; i <= getGameboard().length; i++) {
-
-            }
-        };
-        
-    });
-    
-    // Changes the choosed square in the array location specified
-    // and changes the player turn
-    // const playerMove = (x,y) => {
-    //     if(isPlayerOneTurn) {
-            
-    //         Gameboard.setCell(x, y, p1.getSymbol())
-    //     } else {
-    //         Gameboard.setCell(x, y, p2.getSymbol())
-    //     }
-    //     isPlayerOneTurn = !isPlayerOneTurn;
-    //     return {playerMove}
-    // };
-    const p1 = playerFactory('Player one', 'X');
-    const p2 = playerFactory('Player two', 'O');
-
-    
-    
-    
-
-
-    return {winningCombinations, displayController};
-})();
-
-
-// console.log(Game.winningCombinations);
-// Game.playerMove(2,2);
-// Game.playerMove(1,1);
-
-
-Game.displayController();
