ca4b895afc8cdcaa017e80311f94fb6007f795df
eSuardOdin <esuardodin@gmail.com>
2023-01-18 21:11:25 +0100

    Architecture successfully redesigned

--- script.js
+++ script.js
@@ -3,20 +3,23 @@
 const Game = (() => {
 
     const winningCombinations = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,5,9],
-        [3,5,7],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
         [1,4,7],
-        [2,5,8],
-        [3,6,9]
+        [2,5,8]
     ];
     let isPlayerOneTurn = true;
     let winner = '';
 
+    /**
+     * Returns the gameboard object and accessing methods
+     */
     const Gameboard = (() => {
-        let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     
         const getGameboard = () => {
             return gameboard;
@@ -24,54 +27,14 @@ const Game = (() => {
         const setCell = (x,symbol) =>{
             const htmlPosId = gameboard[x];
             gameboard[x] = symbol;
+            console.log(gameboard);
             return htmlPosId;
         }; 
-        const resetBoard = () => gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+        const resetBoard = () => gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]; 
         return {getGameboard, setCell, resetBoard}
     })();
     
 
-
-
-    const GameLogic =(() => {
-
-        /**
-         * Checks winning combinations against a player's moves played, set the winner if matching
-         * @param {*} player Player object who's moves are checked if matching with any winning combination
-         */
-        const checkWinner = (player) => {
-            winningCombinations.forEach(combination => 
-                {
-                    if (player.getMovesPlayed().indexOf(combination[0]) !== -1 &&
-                    player.getMovesPlayed().indexOf(combination[1]) !== -1 &&
-                    player.getMovesPlayed().indexOf(combination[2]) !== -1) 
-                    {
-                        player.setIsWinner();
-                        winner = player.getName();
-                        console.log(`${player.getName()} wins !`);
-                        return;
-                    }
-
-            });
-            console.log(player);
-        }
-        
-        const playTurn = (square) => {
-            if(winner !== '') return;
-            const player = isPlayerOneTurn ? p1 : p2;
-            if(Gameboard.getGameboard().indexOf(Number(square.getAttribute('id'))) !== -1 && square.innerHTML === '') {
-                
-                square.innerHTML = player.getSymbol();
-                player.addMove(Number(square.getAttribute('id')));
-                checkWinner(player);
-                isPlayerOneTurn = ! isPlayerOneTurn;
-            }
-
-        };
-
-        return {checkWinner,playTurn};
-    }) (); 
-    
     /**
      * 
      * @param {*} name Name of player (optionnal)
@@ -80,28 +43,12 @@ const Game = (() => {
      */
     const playerFactory = (name, symbol) => {
         let _isWinner = false;
-        // Check if win (maybe not legitimate to be there)
-
-        // const checkIfWin = () => {
-        //     winningCombinations.forEach(combination => {
-        //         if (_movesPlayed.indexOf(combination[0]) !== -1 &&
-        //             _movesPlayed.indexOf(combination[1]) !== -1 &&
-        //             _movesPlayed.indexOf(combination[2]) !== -1) 
-        //             {
-        //                 _isWinner = true;
-        //                 console.log(`${getName()} wins !`);
-        //                 return;
-        //         }
-
-        //     });
-        //     if (!_isWinner) console.log(`${getName()} not winning yet`);
-        // }
-
         
         // An array of moves the player did
         const _movesPlayed = [];
         const getMovesPlayed = () => _movesPlayed; // Just for debug
-        // If the player 
+     
+        
         const addMove = (squareNb) => _movesPlayed.push(squareNb);
         const _name = name;
         const getName = () => {
@@ -116,6 +63,111 @@ const Game = (() => {
         }
         return {getName, getSymbol, addMove, getMovesPlayed, setIsWinner};
     }
+    
+
+    /**
+     * Return the GameLogic used to play the game with no visuals
+     */
+    const GameLogic =(() => {
+
+        // Init the 2 players
+        const p1 = playerFactory('Player One', 'X');
+        const p2 = playerFactory('Player Two', 'O');
+
+
+        /**
+         * Checks winning combinations against a player's moves played, set the winner if matching
+         * @param {*} player Player object who's moves are checked if matching with any winning combination
+         */
+        const checkWinner = (player) => {
+            winningCombinations.forEach(combination => 
+                {
+                    if (player.getMovesPlayed().indexOf(combination[0]) !== -1 &&
+                    player.getMovesPlayed().indexOf(combination[1]) !== -1 &&
+                    player.getMovesPlayed().indexOf(combination[2]) !== -1) 
+                    {
+                        player.setIsWinner();
+                        // Set Game.winner to the player name
+                        winner = player.getName();
+                        console.log(`${player.getName()} wins !`);
+                        return;
+                    }
+
+            });
+        }
+        
+        const playMove = (pick, player) => {
+            // Add the move to the player's movelist
+            player.addMove(Gameboard.getGameboard()[pick]);
+            // Change the gameboard cell with player's symbol
+            Gameboard.setCell(pick, player.getSymbol());
+        } 
+
+
+        const playTurn = (pick) => { // Pick will be the square id
+            // Checking if there is no winner yet and setting who's playing
+            if(winner !== '') return;
+            const player = isPlayerOneTurn ? p1 : p2;
+            // If pick is not yet played :
+            //      - the player picks it
+            //      - checking if it makes him win
+            //      - changing turn
+            if(Gameboard.getGameboard()[pick] === pick) { 
+                
+                playMove(pick, player);
+                checkWinner(player);
+                isPlayerOneTurn = ! isPlayerOneTurn;
+            }
+
+        };
+
+        return {playTurn};
+    }) (); 
+    
+    const playLoop = () => {
+        while(winner === '') GameLogic.playTurn(Number(prompt('Enter a number')));
+    }
+
+    const fakeGame = () => {
+        GameLogic.playTurn(1);
+        GameLogic.playTurn(5);
+        GameLogic.playTurn(0);
+        GameLogic.playTurn(8);
+        GameLogic.playTurn(2);
+    }
+    return {fakeGame};
+})();
+
+Game.fakeGame();
+// console.log(Game.winningCombinations);
+// Game.playerMove(2,2);
+// Game.playerMove(1,1);
+
+
+// Game.displayController();
+
+
+
+
+
+
+
+
+
+    
+        // const gameContainer = document.querySelector('.game-container');
+        // // On Game.Reset (?) or new game
+        // const createBoard = () => {
+        //     let html = '';
+        //     Gameboard.getGameboard().forEach(el => {
+        //         console.log(el);
+        //         html += `<span id="${el}" class="square"></span>`;
+        //     });
+        //     gameContainer.insertAdjacentHTML("beforeend", html);
+        // }
+        // createBoard();
+
+
 
     /**
      * For visual display of the game
@@ -166,33 +218,3 @@ const Game = (() => {
 
 
 //     return {winningCombinations, displayController};
-})();
-
-
-// console.log(Game.winningCombinations);
-// Game.playerMove(2,2);
-// Game.playerMove(1,1);
-
-
-// Game.displayController();
-
-
-
-
-
-
-
-
-
-    
-        // const gameContainer = document.querySelector('.game-container');
-        // // On Game.Reset (?) or new game
-        // const createBoard = () => {
-        //     let html = '';
-        //     Gameboard.getGameboard().forEach(el => {
-        //         console.log(el);
-        //         html += `<span id="${el}" class="square"></span>`;
-        //     });
-        //     gameContainer.insertAdjacentHTML("beforeend", html);
-        // }
-        // createBoard();
